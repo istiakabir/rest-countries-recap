bcded7c747f80371b87c89053716b032b30b2ed4
istiakabir~ <istiakabirtrofder@gmail.com>
2022-03-16 16:07:06 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayData(data))
+};
+
+const displayData = countries => {
+    
+}
+
+
+loadCountries();
